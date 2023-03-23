e0c5edffe2da69b822881868110f6a0ce29ec0ea
GnatykOleg <gnaticoleg@gmail.com>
2023-03-23 19:37:12 +0200

    bugfix: Add trycatch to service Add validation for only numeric id

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,6 @@
 import { errorMessages } from '../../common/constants/error-messages';
 import {
+  BadRequestException,
   ConflictException,
   Injectable,
   NotFoundException,
@@ -31,95 +32,116 @@ export class UsersService {
   // Create User
 
   async createUserService(createDTO: CreateUserDTO): Promise<Profile> {
-    // Destructuring the values ​​from createDTO
-    const { firstName, lastName, state, email, username, role } = createDTO;
+    try {
+      // Destructuring the values ​​from createDTO
+      const { firstName, lastName, state, email, username, role } = createDTO;
 
-    // Search user by email
-    const findUserByEmail = await this.usersRepository.findOne({
-      where: { email },
-    });
+      // Search user by email
+      const findUserByEmail = await this.usersRepository.findOne({
+        where: { email },
+      });
 
-    // If user exist throw error 409 Conflict, with custom message
-    if (findUserByEmail)
-      throw new ConflictException(errorMessages.USER_EXIST_EMAIL);
+      // If user exist throw error 409 Conflict, with custom message
+      if (findUserByEmail)
+        throw new ConflictException(errorMessages.USER_EXIST_EMAIL);
 
-    // If User doesn't exist, we create user
+      // If User doesn't exist, we create user
 
-    // Create a profile with the desired values
-    const createProfile = await this.profilesRepository.create({
-      firstName,
-      lastName,
-      state,
-    });
+      // Create a profile with the desired values
+      const createProfile = await this.profilesRepository.create({
+        firstName,
+        lastName,
+        state,
+      });
 
-    // Create a user with the desired values
-    await this.usersRepository.create({
-      email,
-      username,
-      role,
-      profileId: createProfile.id,
-    });
+      // Create a user with the desired values
+      await this.usersRepository.create({
+        email,
+        username,
+        role,
+        profileId: createProfile.id,
+      });
 
-    // Get additional user values
-    const createdUser = await this.findUserById(createProfile.id);
+      // Get additional user values
+      const createdUser = await this.findUserById(createProfile.id);
 
-    // Return addition info after update
-    return createdUser;
+      // Return addition info after update
+      return createdUser;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
   // Update User
 
   async updateUserService({ id, updateDTO }: UpdateProps): Promise<Profile> {
-    // Destructuring the values ​​from updateDTO
-    const { firstName, lastName, state, email, username, role } = updateDTO;
+    try {
+      // Destructuring the values ​​from updateDTO
+      const { firstName, lastName, state, email, username, role } = updateDTO;
 
-    const findUser = await this.findUserById(Number(id));
-    if (!findUser) throw new NotFoundException(errorMessages.USER_EXIST_ID);
+      const findUser = await this.findUserById(Number(id));
+      if (!findUser) throw new NotFoundException(errorMessages.USER_EXIST_ID);
 
-    // Update profile data
-    await this.profilesRepository.update(
-      { firstName, lastName, state },
-      { where: { id } },
-    );
+      // Update profile data
+      await this.profilesRepository.update(
+        { firstName, lastName, state },
+        { where: { id } },
+      );
 
-    // Update user data
-    await this.usersRepository.update(
-      { email, username, role },
-      { where: { id } },
-    );
+      // Update user data
+      await this.usersRepository.update(
+        { email, username, role },
+        { where: { id } },
+      );
 
-    // Return addition info after update
-    return await this.findUserById(Number(id));
+      // Return addition info after update
+      return await this.findUserById(Number(id));
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   // Get All Users
 
   async getAllUsersService(): Promise<Profile[]> {
-    const usersToFind = await this.profilesRepository.findAll({
-      include: {
-        model: User,
-      },
-    });
-    return usersToFind;
+    try {
+      const usersToFind = await this.profilesRepository.findAll({
+        include: {
+          model: User,
+        },
+      });
+      return usersToFind;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   // Get Users By Role
 
   async getUsersByRole(role: UserRole): Promise<Profile[]> {
-    const usersByRole = await this.profilesRepository.findAll({
-      include: {
-        model: User,
-        where: { role },
-      },
-    });
+    try {
+      const usersByRole = await this.profilesRepository.findAll({
+        include: {
+          model: User,
+          where: { role },
+        },
+      });
 
-    return usersByRole;
+      return usersByRole;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   // Delete User
 
   async deleteUserService(id: string): Promise<void> {
-    const findUserById = await this.findUserById(Number(id));
-    if (!findUserById) throw new NotFoundException(errorMessages.USER_EXIST_ID);
-    await this.profilesRepository.destroy({ where: { id } });
+    try {
+      const findUserById = await this.findUserById(Number(id));
+      if (!findUserById)
+        throw new NotFoundException(errorMessages.USER_EXIST_ID);
+      await this.profilesRepository.destroy({ where: { id } });
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 }
